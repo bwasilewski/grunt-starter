9bd65314cf9232009d0af31f0d6200fc66fca4c0
Benjamin Wasilewski <benwasilewski@gmail.com>
2013-12-08 15:12:19 -0800

    updated gruntfile with jshint functionality

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,34 @@
 module.exports = function (grunt) {
-	
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		uglify: {
-			build: {
+  
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      all: ['Gruntfile.js', 'js/scripts.js']
+    },
+    
+    uglify: {
+      build: {
         src: 'js/scripts.js',
         dest: 'js/scripts.min.js'
       }
-		}
-	});
+    }
+  });
 
-	// Load the "Uglify" plugin
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+  // Load the jshint plugin
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  // Load the uglify plugin
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	// Set up our default task
-	grunt.registerTask('default', ['uglify']);
+  // Set up our default task
+  grunt.registerTask('default', ['jshint', 'uglify']);
 };
